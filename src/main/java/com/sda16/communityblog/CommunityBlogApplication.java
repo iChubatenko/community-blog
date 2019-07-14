591e72b0d8675a402a48bbb430660e3e885c57ac
Ihor Chubatenko <doubleredbull@gmail.com>
2019-07-14 11:39:16 +0200

    First Commit

---
+++ src/main/java/com/sda16/communityblog/CommunityBlogApplication.java
@@ -0,0 +1,13 @@
+package com.sda16.communityblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityBlogApplication.class, args);
+	}
+
+}
