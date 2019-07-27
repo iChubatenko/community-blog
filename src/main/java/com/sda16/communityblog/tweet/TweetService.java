94a14639cef01e94c2274ba8daa1bdc1efd16b41
Ihor Chubatenko <doubleredbull@gmail.com>
2019-07-27 10:38:14 +0200

    Make tweet page.

--- src/main/java/com/sda16/communityblog/tweet/TweetService.java
+++ src/main/java/com/sda16/communityblog/tweet/TweetService.java
@@ -1,10 +1,15 @@
 package com.sda16.communityblog.tweet;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TweetService {
 
+    private TweetRepository tweetRepository;
+
+    TweetEntity tweet = new TweetEntity();
 
 
 }
